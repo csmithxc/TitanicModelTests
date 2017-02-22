1c37fe21d4fa8d19325cf921158d1acf5f19b5c1
Cole Smith <cole.f.smith1@gmail.com>
2017-02-22 09:04:55 -0600

    Titanic NNet mk3
    Improved age factors, and new family factor

--- TitanicSmuckersData.R
+++ TitanicSmuckersData.R
@@ -9,19 +9,22 @@ test_data <- titanic_test
 
 
 #Convert age to binary child
-train_data <- mutate(train_data, Child = ifelse(Age < 18, 1, 0))
-test_data <- mutate(test_data, Child = ifelse(Age < 18, 1, 0))
+train_data <- mutate(train_data, Child = ifelse(Age < 18, 1, ifelse(Age > 60, 2, 0)))
+test_data <- mutate(test_data, Child = ifelse(Age < 18, 1, ifelse(Age > 45, 2, 0)))
 train_data$Child[is.na(train_data$Child)] <- ifelse(grepl("Master", train_data$Name[is.na(train_data$Child)]), 1, ifelse(grepl("Miss", train_data$Name[is.na(train_data$Child)]), 1, 0)) 
 test_data$Child[is.na(test_data$Child)] <- ifelse(grepl("Master", test_data$Name[is.na(test_data$Child)]), 1, ifelse(grepl("Miss", test_data$Name[is.na(test_data$Child)]), 1, 0)) 
 train_data <- mutate(train_data, Level = substr(Cabin, 1, 1))
 test_data <- mutate(test_data, Level = substr(Cabin, 1, 1))
+train_data <- mutate(train_data, Family = ifelse(Parch > 1, 1, ifelse(SibSp > 0, 1, 0)))
+test_data <- mutate(test_data, Family = ifelse(Parch > 1, 1, ifelse(SibSp > 0, 1, 0)))
+
 
 
 #setting seed allows repeatable results
 set.seed(2)
 
 #create neural net
-nnet.fit <- nnet(Survived ~ factor(Pclass)+Sex+factor(Child) + factor(Level), data = train_data, size = 3)
+nnet.fit <- nnet(Survived ~ factor(Pclass)+Sex+factor(Child)+factor(Family), data = train_data, size = 3)
 
 #predict who survived
 predicted <- predict(nnet.fit, test_data)
@@ -44,4 +47,4 @@ confusionMatrix(as.factor(predicted$predicted),
 predicted <- data.frame(test_data[c(1:418), 1], predicted$predicted)
 colnames(predicted)[1] <- "PassengerId"
 colnames(predicted)[2] <- "Survived"
-write.csv(predicted, "NNetTest3.csv", row.names = FALSE)
+#write.csv(predicted, "NNetTest6.csv", row.names = FALSE)
